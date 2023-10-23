b015fdb2d1cd7e9ad7ea75ceb52a6dc24e8b5270
Devonte <devo.tox.89@gmail.com>
2023-10-23 13:01:14 +0000

    remove semi colons

--- lib/strip-slash.ts
+++ lib/strip-slash.ts
@@ -1,28 +1,28 @@
-import { Context } from 'https://edge.netlify.com';
+import { Context } from 'https://edge.netlify.com'
 
 export default async function handler(request: Request, context: Context) {
-  const { pathname } = new URL(request.url);
+  const { pathname } = new URL(request.url)
   // Skip for root, or if we're already proxying the request
   if (pathname === '/' || request.headers.get('x-nf-subrequest')) {
-    return;
+    return
   }
 
   // Redirect to remove the trailing slash or .html
   for (const suffix of ['/', '/index.html', '.html']) {
     if (pathname.endsWith(suffix)) {
-      return Response.redirect(request.url.slice(0, -suffix.length), 301);
+      return Response.redirect(request.url.slice(0, -suffix.length), 301)
     }
   }
 
-  const response = await context.next({ sendConditionalRequest: true });
+  const response = await context.next({ sendConditionalRequest: true })
 
   // If origin returns a 301 we need to proxy it to avoid a redirect loop
   if (response.status === 301 && pathname.endsWith('/')) {
-    const location = response.headers.get('Location');
+    const location = response.headers.get('Location')
     // Avoid infinite loops
-    request.headers.set('x-nf-subrequest', '1');
-    return context.rewrite(new URL(location || '', request.url).toString());
+    request.headers.set('x-nf-subrequest', '1')
+    return context.rewrite(new URL(location || '', request.url).toString())
   }
 
-  return response;
+  return response
 }
