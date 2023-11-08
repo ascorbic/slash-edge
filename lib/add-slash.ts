ecb71741214258a5bd0c2c8cf2a1459feff17cd7
Devonte <devo.tox.89@gmail.com>
2023-11-08 11:39:09 +0000

    fix: handle Query String and allow some redirects (#4)
    * Update add-slash.ts
    
    * update strip slash
    
    * update quotes
    
    * remove semi colons
    
    * Take search into account
    
    * Consider search
    
    * Update strip-slash.ts
    
    * Update add-slash.ts

--- lib/add-slash.ts
+++ lib/add-slash.ts
@@ -1,33 +1,44 @@
 import { Context } from 'https://edge.netlify.com'
 
 export default async function handler(request: Request, context: Context) {
-  const { pathname } = new URL(request.url)
+  const url = new URL(request.url)
+  const { pathname, search } = url
 
   // Skip if we're already proxying the request
-  if (request.headers.get('x-nf-subrequest')) {
+  if (pathname === '/' || request.headers.get('x-nf-sub-request')) {
     return
   }
 
   // Redirect to remove the .html
   for (const suffix of ['/index.html', '.html']) {
     if (pathname.endsWith(suffix)) {
-      // Replace the suffix with a slash
-      return Response.redirect(`${request.url.slice(0, -suffix.length)}/`, 301)
+      // Replace the suffix with a slash and append the query string
+      const newLocation = `${url.origin}${pathname.slice(
+        0,
+        -suffix.length
+      )}/${search}`
+
+      return Response.redirect(newLocation, 301)
     }
   }
-  // Add a slash, unless there's a file extension
+  // Add a slash if there's no file extension, and append the query string
   if (!pathname.endsWith('/') && !pathname.split('/').pop()?.includes('.')) {
-    return Response.redirect(`${request.url}/`, 301)
+    const newLocation = `${url.origin}${pathname}/${search}`
+    return Response.redirect(newLocation, 301)
   }
 
   const response = await context.next({ sendConditionalRequest: true })
+
   // If origin returns a 301 we need to proxy it to avoid a redirect loop
-  // TODO: check that this is just a redirect to the canonical URL, not some other kind
-  if (response.status === 301) {
+  if (response.status === 301 && !pathname.endsWith('/')) {
     const location = response.headers.get('Location')
     // Avoid infinite loops
-    request.headers.set('x-nf-subrequest', '1')
-    return context.rewrite(new URL(location || '', request.url).toString())
+    request.headers.set('x-nf-sub-request', '1')
+    // Ensure the query string is included in the rewritten location
+    const newLocation = new URL(location || '', request.url)
+    newLocation.search = search // Preserve the query string
+    return context.rewrite(newLocation.toString())
   }
+
   return response
 }
--- lib/strip-slash.ts
+++ lib/strip-slash.ts
@@ -1,28 +1,36 @@
 import { Context } from 'https://edge.netlify.com'
 
 export default async function handler(request: Request, context: Context) {
-  const { pathname } = new URL(request.url)
+  const url = new URL(request.url)
+  const { pathname, search } = url
+
   // Skip for root, or if we're already proxying the request
-  if (pathname === '/' || request.headers.get('x-nf-subrequest')) {
+  if (pathname === '/' || request.headers.get('x-nf-sub-request')) {
     return
   }
 
   // Redirect to remove the trailing slash or .html
   for (const suffix of ['/', '/index.html', '.html']) {
     if (pathname.endsWith(suffix)) {
-      return Response.redirect(request.url.slice(0, -suffix.length), 301)
+      // Construct the new URL without the suffix and with the query string
+      const newLocation = `${url.origin}${pathname.slice(
+        0,
+        -suffix.length
+      )}${search}`
+
+      return Response.redirect(newLocation, 301)
     }
   }
 
   const response = await context.next({ sendConditionalRequest: true })
 
   // If origin returns a 301 we need to proxy it to avoid a redirect loop
-  // TODO: check that this is just a redirect to the canonical URL, not some other kind
-  if (response.status === 301) {
+  if (response.status === 301 && pathname.endsWith('/')) {
     const location = response.headers.get('Location')
     // Avoid infinite loops
-    request.headers.set('x-nf-subrequest', '1')
+    request.headers.set('x-nf-sub-request', '1')
     return context.rewrite(new URL(location || '', request.url).toString())
   }
+
   return response
 }
